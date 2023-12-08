fbfa9d9f8fcfe87cd3bec832eafc26f828213f7e
Muh1isa <lisaa27.a6@gmail.com>
2023-12-08 14:21:16 +0500

    drinks added to cards

--- src/data.jsx
+++ src/data.jsx
@@ -42,4 +42,28 @@ export const data = [
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, deleniti.',
         img: 'https://i.ibb.co/Xz2Pv4S/cb33c85fc29f3b50c46fc2e6f562eb36-removebg-preview.png'
     },
+    {
+        id: 6,
+        category: 'drink',
+        name: 'Sutli kofe',
+        price: '7 000',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, deleniti.',
+        img: 'https://i.ibb.co/5hDLGzn/699d5d6f-7c16-47db-bdda-fd9e0fb484c7.png'
+    },
+    {
+        id: 7,
+        category: 'drink',
+        name: 'Kofe',
+        price: '7 000',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, deleniti.',
+        img: 'https://i.ibb.co/GHFhdSM/e0da094b-d6f2-420a-91a2-03fe73c88b04.jpg'
+    },
+    {
+        id: 8,
+        category: 'drink',
+        name: 'Choy',
+        price: '3 000',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, deleniti.',
+        img: 'https://i.ibb.co/r6hXRjW/tea.png'
+    },
 ]
